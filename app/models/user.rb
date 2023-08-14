4344e565ab1e69425c8b70e0118da554ad54d532
Emilia <emilia.skrzypczak.es@gmail.com>
2023-08-14 20:46:25 +0200

    allow user to sign-in via either email or phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+
   validates :phone_number, uniqueness: true, unless: -> { phone_number.blank? }
+
+  attr_writer :login
+
+
+  def login
+    @login || phone_number || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['phone_number = ? OR email = ?', login.to_i, login.to_s.downcase]).first
+    elsif conditions.has_key?(:phone_number) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
