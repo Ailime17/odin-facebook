95a87c745720f1cb909616c98ce79da941bb683d
Emilia <emilia.skrzypczak.es@gmail.com>
2023-08-27 12:14:57 +0200

    improve friend_requests routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   root 'users#show'
 
   resources :users, only: :show do
-    resources :friend_requests, only: [:create, :update, :destroy]
+    resources :friend_requests, only: [:create]
     resources :posts, only: [:new, :create]
   end
   resources :posts, only: [:destroy, :show] do
     resources :likes, only: [:create]
     resources :comments, only: [:create]
   end
+  resources :friend_requests, only: [:update, :destroy]
   resources :likes, only: [:destroy]
   resources :comments, only: [:destroy]
 end
