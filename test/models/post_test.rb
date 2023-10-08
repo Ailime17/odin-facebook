69bcb73ea9bd07f3de3d44b35de98e63b8224659
Emilia <emilia.skrzypczak.es@gmail.com>
2023-10-08 18:02:01 +0200

    add unit tests for Post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post_one = posts(:one)
+  end
+
+  test 'Post-User association works properly' do
+    @user_one = users(:one)
+    assert @post_one.user == @user_one
+  end
+
+  test 'Post-Like association works properly' do
+    @like_one = likes(:one)
+    assert @post_one.likes.exists?(@like_one.id)
+    @user_one = users(:one)
+    assert @post_one.liking_users.exists?(@user_one.id)
+  end
+
+  test 'Post-Comment association works properly' do
+    @comment_one = comments(:one)
+    assert @post_one.comments.exists?(@comment_one.id)
+  end
 end
