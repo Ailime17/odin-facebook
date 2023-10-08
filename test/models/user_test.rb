4e696a357c976ca8b9d204db252ebfdbe3423b83
Emilia <emilia.skrzypczak.es@gmail.com>
2023-10-08 19:48:37 +0200

    refactor unit tests for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,21 +8,18 @@ class UserTest < ActiveSupport::TestCase
   test 'User-Post association works properly' do
     @post_one = posts(:one)
     assert @user_one.posts.exists?(@post_one.id)
-    assert @post_one.user == @user_one
   end
 
   test 'User-Like association works properly' do
     @post_one = posts(:one)
     @like_one = likes(:one)
     assert @user_one.likes.exists?(@like_one.id)
-    assert @like_one.user == @user_one
     assert @user_one.liked_posts.exists?(@post_one.id)
   end
 
   test 'User-Comment association works properly' do
     @comment_one = comments(:one)
     assert @user_one.comments.exists?(@comment_one.id)
-    assert @comment_one.user == @user_one
   end
 
   test 'User-Friend_Request association works properly' do
@@ -30,6 +27,5 @@ class UserTest < ActiveSupport::TestCase
     @user_two = users(:two)
     assert @user_one.sent_friend_requests.exists?(@fr_req_one.id)
     assert @user_two.received_friend_requests.exists?(@fr_req_one.id)
-    assert @fr_req_one.sender == @user_one && @fr_req_one.receiver == @user_two
   end
 end
