ab35730c0e5a30bfeed78952c9c049dc078eb50a
Emilia <emilia.skrzypczak.es@gmail.com>
2023-10-08 18:18:27 +0200

    add unit tests for Like

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @like_one = likes(:one)
+  end
+
+  test 'Like-User association works properly' do
+    @user_one = users(:one)
+    assert @like_one.user == @user_one
+  end
+
+  test 'Like-Post association works properly' do
+    @post_one = posts(:one)
+    assert @like_one.post == @post_one
+  end
 end
