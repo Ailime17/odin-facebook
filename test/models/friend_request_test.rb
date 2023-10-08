897fa3bb31965433fccdfd498d06027aba7c9bc8
Emilia <emilia.skrzypczak.es@gmail.com>
2023-10-08 18:42:23 +0200

    add unit tests for FriendRequest

--- test/models/friend_request_test.rb
+++ test/models/friend_request_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class FriendRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @fr_req_one = friend_requests(:one)
+  end
+
+  test 'Friend_Request-User association works properly' do
+    @user_one = users(:one)
+    @user_two = users(:two)
+    assert @fr_req_one.sender == @user_one && @fr_req_one.receiver == @user_two
+  end
+
+  test 'Friend_Request-Friend_Request_Status association works properly' do
+    @fr_req_status_one = friend_request_statuses(:one)
+    assert @fr_req_one.status == @fr_req_status_one
+  end
 end
