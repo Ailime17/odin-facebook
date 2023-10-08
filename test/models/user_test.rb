e5cea3f87b5dc263c5583c7982d91978ff86645d
Emilia <emilia.skrzypczak.es@gmail.com>
2023-10-08 17:32:49 +0200

    correct fixtures and add unit tests for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_one = users(:one)
+  end
+
+  test 'User-Post association works properly' do
+    @post_one = posts(:one)
+    assert @user_one.posts.exists?(@post_one.id)
+    assert @post_one.user == @user_one
+  end
+
+  test 'User-Like association works properly' do
+    @post_one = posts(:one)
+    @like_one = likes(:one)
+    assert @user_one.likes.exists?(@like_one.id)
+    assert @like_one.user == @user_one
+    assert @user_one.liked_posts.exists?(@post_one.id)
+  end
+
+  test 'User-Comment association works properly' do
+    @comment_one = comments(:one)
+    assert @user_one.comments.exists?(@comment_one.id)
+    assert @comment_one.user == @user_one
+  end
+
+  test 'User-Friend_Request association works properly' do
+    @fr_req_one = friend_requests(:one)
+    @user_two = users(:two)
+    assert @user_one.sent_friend_requests.exists?(@fr_req_one.id)
+    assert @user_two.received_friend_requests.exists?(@fr_req_one.id)
+    assert @fr_req_one.sender == @user_one && @fr_req_one.receiver == @user_two
+  end
 end
